fd264a48563a7993c8a1548d432cb65ee7bf5a11
Milad Vahdatkhah <mvahdatkhah.work@gmail.com>
2024-08-13 13:47:58 +0330

    add security group for Firewall configuration

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ variable "vpc_cidr_block" {}
 variable "subnet_cidr_block" {}
 variable "avail_zone" {}
 variable "env_prefix" {}
+variable "my_ip_address" {}
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
@@ -39,4 +40,36 @@ resource "aws_default_route_table" "main-rtb" {
   tags = {
     Name : "${var.env_prefix}-main-rtb"
   }
+}
+
+resource "aws_security_group" "myapp-sg" {
+  name   = "myapp-sg"
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip_address]
+  }
+
+  ingress {
+    from_port = 8080
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = -1
+    cidr_blocks = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+
+  tags = {
+    Name : "${var.env_prefix}-sg"
+  }
+
 }
