af39472f484f692d537c140c03e5341444bcaacc
Milad Vahdatkhah <mvahdatkhah.work@gmail.com>
2024-08-13 11:27:48 +0330

    add default route table resource

--- main.tf
+++ main.tf
@@ -25,6 +25,7 @@ resource "aws_subnet" "myapp-subnet-1" {
 
 /*resource "aws_route_table" "myapp-route-table" {
   vpc_id = aws_vpc.myapp-vpc.id
+
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.myapp-igw.id
@@ -38,6 +39,18 @@ resource "aws_internet_gateway" "myapp-igw" {
   vpc_id = aws_vpc.myapp-vpc.id
   tags = {
     Name : "${var.env_prefix}-igw"
+  }
+}
+
+resource "aws_default_route_table" "main-rtb" {
+  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+  }
+  tags = {
+    Name : "${var.env_prefix}-main-rtb"
   }
 }
 
