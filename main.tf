ee2aed34cc46169ac1d73eff31dbc99956245786
Milad Vahdatkhah <mvahdatkhah.work@gmail.com>
2024-08-14 10:51:39 +0330

    edit aws_key_pair of aws_key_pair resource

--- main.tf
+++ main.tf
@@ -97,7 +97,7 @@ output "aws_ami_id" {
 
 resource "aws_key_pair" "ssh-key" {
   key_name = "server-key"  
-  public_key = "${file(var.public_key_location)}"
+  public_key = file(var.public_key_location)
 }
 
 resource "aws_instance" "myapp-server" {
