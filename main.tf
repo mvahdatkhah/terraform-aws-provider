e1834c22dfabfc612b7681f18577b3a997b57a26
Milad Vahdatkhah <mvahdatkhah.work@gmail.com>
2024-08-14 11:00:54 +0330

    edit value of aws_ami_id output name

--- main.tf
+++ main.tf
@@ -96,7 +96,7 @@ output "aws_ami_id" {
 }
 
 output "ec2_public_ip" {
-  value = aws_instance.myapp-server  
+  value = aws_instance.myapp-server.public_ip  
 }
 resource "aws_key_pair" "ssh-key" {
   key_name = "server-key"  
