1f2d78b50c35a0540d9f56dc7c2234c1e94ff7d8
Milad Vahdatkhah <mvahdatkhah.work@gmail.com>
2024-08-14 10:57:40 +0330

    add ec2_public_ip output

--- main.tf
+++ main.tf
@@ -95,6 +95,9 @@ output "aws_ami_id" {
 
 }
 
+output "ec2_public_ip" {
+  value = aws_instance.myapp-server  
+}
 resource "aws_key_pair" "ssh-key" {
   key_name = "server-key"  
   public_key = file(var.public_key_location)
