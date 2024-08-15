2f5a1ea4bd300d9f60406986fd365c1ce4292a39
Milad Vahdatkhah <mvahdatkhah.work@gmail.com>
2024-08-15 10:53:31 +0330

    re-order remote-exe, local-exec and file provisioner

--- main.tf
+++ main.tf
@@ -9,11 +9,11 @@ variable "env_prefix" {}
 variable "my_ip_address" {}
 variable "instance_type" {}
 variable "public_key_location" {
-  type    = string
+  type = string
 }
 
 variable "private_key_location" {
-  type = string  
+  type = string
 }
 
 resource "aws_vpc" "myapp-vpc" {
@@ -105,7 +105,7 @@ output "ec2_public_ip" {
   value = aws_instance.myapp-server.public_ip
 }
 resource "aws_key_pair" "ssh-key" {
-  key_name = "server-key"
+  key_name   = "server-key"
   public_key = file(var.public_key_location)
 }
 
@@ -113,20 +113,25 @@ resource "aws_instance" "myapp-server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
 
-  subnet_id = aws_subnet.myapp-subnet-1.id
+  subnet_id              = aws_subnet.myapp-subnet-1.id
   vpc_security_group_ids = [aws_default_security_group.myapp-sg.id]
-  availability_zone = var.avail_zone
+  availability_zone      = var.avail_zone
 
   associate_public_ip_address = true
-  key_name = aws_key_pair.ssh-key.key_name
+  key_name                    = aws_key_pair.ssh-key.key_name
 
   # user_data = file("user_data.sh")
 
   connection {
-    type = "ssh"
-    host = self.public_ip
-    user = "ubuntu"
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ubuntu"
     private_key = file(var.private_key_location)
+  }
+
+  provisioner "file" {
+    source      = "user_data.sh"
+    destination = "/home/ubuntu/user_data_on_ec2.sh"
   }
   provisioner "remote-exec" {
     script = "user_data.sh"
@@ -136,10 +141,7 @@ resource "aws_instance" "myapp-server" {
     command = "echo ${self.public_ip} > output.txt"
   }
 
-  provisioner "file" {
-    source = "user_data.sh"
-    destination = "/home/ubuntu/user_data_on_ec2.sh"
-  }
+
   tags = {
     Name = "${var.env_prefix}-server"
   }
