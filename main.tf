d8ba766240cfe392a5cfeb8fc64bc4d08d22de85
Milad Vahdatkhah <mvahdatkhah.work@gmail.com>
2024-08-15 13:15:24 +0330

    remove redundant sections to keep the code DRY and use aws_security_group instead of aws_default_security_group

--- main.tf
+++ main.tf
@@ -11,7 +11,6 @@ variable "instance_type" {}
 variable "public_key_location" {
   type = string
 }
-
 variable "private_key_location" {
   type = string
 }
@@ -19,7 +18,7 @@ variable "private_key_location" {
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
   tags = {
-    Name : "${var.env_prefix}-vpc"
+    Name = "${var.env_prefix}-vpc"
   }
 }
 
@@ -28,13 +27,14 @@ resource "aws_subnet" "myapp-subnet-1" {
   cidr_block        = var.subnet_cidr_block
   availability_zone = var.avail_zone
   tags = {
-    Name : "${var.env_prefix}-subnet-1"
+    Name = "${var.env_prefix}-subnet-1"
   }
 }
+
 resource "aws_internet_gateway" "myapp-igw" {
   vpc_id = aws_vpc.myapp-vpc.id
   tags = {
-    Name : "${var.env_prefix}-igw"
+    Name = "${var.env_prefix}-igw"
   }
 }
 
@@ -46,12 +46,13 @@ resource "aws_default_route_table" "main-rtb" {
     gateway_id = aws_internet_gateway.myapp-igw.id
   }
   tags = {
-    Name : "${var.env_prefix}-main-rtb"
+    Name = "${var.env_prefix}-main-rtb"
   }
 }
 
-resource "aws_default_security_group" "myapp-sg" {
+resource "aws_security_group" "myapp-sg" {
   vpc_id = aws_vpc.myapp-vpc.id
+  name   = "${var.env_prefix}-sg"
 
   ingress {
     from_port   = 22
@@ -68,23 +69,20 @@ resource "aws_default_security_group" "myapp-sg" {
   }
 
   egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = -1
-    cidr_blocks     = ["0.0.0.0/0"]
-    prefix_list_ids = []
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
-    Name : "${var.env_prefix}-sg"
+    Name = "${var.env_prefix}-sg"
   }
-
 }
-
 
 data "aws_ami" "ubuntu" {
   most_recent = true
-  owners      = ["099720109477"]
+  owners      = ["099720109477"]  # Canonical
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
@@ -96,31 +94,20 @@ data "aws_ami" "ubuntu" {
   }
 }
 
-output "aws_ami_id" {
-  value = data.aws_ami.ubuntu.id
-
-}
-
-output "ec2_public_ip" {
-  value = aws_instance.myapp-server.public_ip
-}
 resource "aws_key_pair" "ssh-key" {
   key_name   = "server-key"
   public_key = file(var.public_key_location)
 }
 
 resource "aws_instance" "myapp-server" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
   subnet_id              = aws_subnet.myapp-subnet-1.id
-  vpc_security_group_ids = [aws_default_security_group.myapp-sg.id]
+  vpc_security_group_ids = [aws_security_group.myapp-sg.id]
   availability_zone      = var.avail_zone
 
   associate_public_ip_address = true
   key_name                    = aws_key_pair.ssh-key.key_name
-
-  # user_data = file("user_data.sh")
 
   connection {
     type        = "ssh"
@@ -133,20 +120,27 @@ resource "aws_instance" "myapp-server" {
     source      = "user_data.sh"
     destination = "/home/ubuntu/user_data_on_ec2.sh"
   }
+
   provisioner "remote-exec" {
     inline = [
-    "chmod +x /home/ubuntu/user_data_on_ec2.sh",
-    "sudo /home/ubuntu/user_data_on_ec2.sh"
+      "chmod +x /home/ubuntu/user_data_on_ec2.sh",
+      "sudo /home/ubuntu/user_data_on_ec2.sh"
     ]
   }
-  
 
   provisioner "local-exec" {
     command = "echo ${self.public_ip} > output.txt"
   }
 
-
   tags = {
     Name = "${var.env_prefix}-server"
   }
+}
+
+output "aws_ami_id" {
+  value = data.aws_ami.ubuntu.id
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.myapp-server.public_ip
 }
