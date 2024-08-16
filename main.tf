75dd98d9b5f444758c52b4f3a94f8f28fd76f3d7
Milad Vahdatkhah <mvahdatkhah.work@gmail.com>
2024-08-16 15:59:19 +0330

    add module 'myapp-subnet' block

--- main.tf
+++ main.tf
@@ -9,32 +9,13 @@ resource "aws_vpc" "myapp-vpc" {
   }
 }
 
-resource "aws_subnet" "myapp-subnet-1" {
-  vpc_id            = aws_vpc.myapp-vpc.id
-  cidr_block        = var.subnet_cidr_block
-  availability_zone = var.avail_zone
-  tags = {
-    Name = "${var.env_prefix}-subnet-1"
-  }
-}
-
-resource "aws_internet_gateway" "myapp-igw" {
+module "myapp-subnet" {
+  source = "./modules/subnet"
+  subnet_cidr_block = var.subnet_cidr_block
+  avail_zone = var.avail_zone
+  env_prefix = var.env_prefix
   vpc_id = aws_vpc.myapp-vpc.id
-  tags = {
-    Name = "${var.env_prefix}-igw"
-  }
-}
-
-resource "aws_default_route_table" "main-rtb" {
-  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.myapp-igw.id
-  }
-  tags = {
-    Name = "${var.env_prefix}-main-rtb"
-  }
+    default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
 }
 
 resource "aws_security_group" "myapp-sg" {
@@ -95,30 +76,6 @@ resource "aws_instance" "myapp-server" {
 
   associate_public_ip_address = true
   key_name                    = aws_key_pair.ssh-key.key_name
-
-  connection {
-    type        = "ssh"
-    host        = self.public_ip
-    user        = "ubuntu"
-    private_key = file(var.private_key_location)
-  }
-
-  provisioner "file" {
-    source      = "user_data.sh"
-    destination = "/home/ubuntu/user_data_on_ec2.sh"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "chmod +x /home/ubuntu/user_data_on_ec2.sh",
-      "/home/ubuntu/user_data_on_ec2.sh"
-    ]
-  }
-
-  provisioner "local-exec" {
-    command = "echo ${self.public_ip} > output.txt"
-  }
-
   tags = {
     Name = "${var.env_prefix}-server"
   }
