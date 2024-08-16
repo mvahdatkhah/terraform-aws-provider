59531cf2f80125c2dd93d03171303988c7ca2044
Milad Vahdatkhah <mvahdatkhah.work@gmail.com>
2024-08-16 14:54:46 +0330

    add variables.tf file

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "vpc_cidr_block" {}
+variable "subnet_cidr_block" {}
+variable "avail_zone" {}
+variable "env_prefix" {}
+variable "my_ip_address" {}
+variable "instance_type" {}
+variable "public_key_location" {
+  type = string
+}
+variable "private_key_location" {
+  type = string
+}
