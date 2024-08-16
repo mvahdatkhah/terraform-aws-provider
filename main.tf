93f9b1c8bdde8da529402a231f8b4e8d6d10563f
Milad Vahdatkhah <mvahdatkhah.work@gmail.com>
2024-08-16 17:39:25 +0330

    add module 'myapp-server' block

--- main.tf
+++ main.tf
@@ -10,74 +10,21 @@ resource "aws_vpc" "myapp-vpc" {
 }
 
 module "myapp-subnet" {
-  source = "./modules/subnet"
-  subnet_cidr_block = var.subnet_cidr_block
-  avail_zone = var.avail_zone
-  env_prefix = var.env_prefix
-  vpc_id = aws_vpc.myapp-vpc.id
-    default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+  source                 = "./modules/subnet"
+  subnet_cidr_block      = var.subnet_cidr_block
+  avail_zone             = var.avail_zone
+  env_prefix             = var.env_prefix
+  vpc_id                 = aws_vpc.myapp-vpc.id
+  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
 }
-
-resource "aws_security_group" "myapp-sg" {
-  vpc_id = aws_vpc.myapp-vpc.id
-  name   = "${var.env_prefix}-sg"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [var.my_ip_address]
-  }
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "${var.env_prefix}-sg"
-  }
+module "myapp-server" {
+  source              = "./modules/webserver"
+  vpc_id              = aws_vpc.myapp-vpc.id
+  my_ip_address       = var.my_ip_address
+  env_prefix          = var.env_prefix
+  image_name          = var.image_name
+  public_key_location = var.public_key_location
+  instance_type       = var.instance_type
+  subnet_id           = module.myapp-subnet.subnet.id
+  avail_zone          = var.avail_zone
 }
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners      = ["099720109477"]  # Canonical
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
-resource "aws_key_pair" "ssh-key" {
-  key_name   = "server-key"
-  public_key = file(var.public_key_location)
-}
-
-resource "aws_instance" "myapp-server" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = var.instance_type
-  subnet_id              = module.myapp-subnet.subnet.id
-  vpc_security_group_ids = [aws_security_group.myapp-sg.id]
-  availability_zone      = var.avail_zone
-
-  associate_public_ip_address = true
-  key_name                    = aws_key_pair.ssh-key.key_name
-  tags = {
-    Name = "${var.env_prefix}-server"
-  }
-}
-
