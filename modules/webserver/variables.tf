cd29fe544141b98b88bb9c3312ec72bf128dff0c
Milad Vahdatkhah <mvahdatkhah.work@gmail.com>
2024-08-16 17:40:24 +0330

    add modules/subnet/variables.tf file

---
+++ modules/webserver/variables.tf
@@ -0,0 +1,10 @@
+variable vpc_id {}
+variable my_ip_address {}
+variable env_prefix {}
+variable image_name {}
+variable "public_key_location" {
+  type = string
+}
+variable instance_type {}
+variable subnet_id {}
+variable avail_zone {}
