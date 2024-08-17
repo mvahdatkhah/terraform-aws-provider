afacb4b401de2e6b12c1febaeba56aae8df2e159
Milad Vahdatkhah <mvahdatkhah.work@gmail.com>
2024-08-17 07:39:09 +0330

    edit vpc module and refrence to myapp-server module

--- main.tf
+++ main.tf
@@ -6,28 +6,24 @@ module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "my-vpc"
-  cidr = "10.0.0.0/16"
+  cidr = var.vpc_cidr_block
 
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  enable_nat_gateway = true
-  enable_vpn_gateway = true
+  azs             = [var.avail_zone]
+  public_subnets  = [var.subnet_cidr_block]
+  public_subnet_tags = { Name: "${var.env_prefix}-subnet-1" }
 
   tags = {
-    Terraform = "true"
-    Environment = "dev"
+    Name = "${var.env_prefix}-vpc"
   }
 }
 module "myapp-server" {
   source              = "./modules/webserver"
-  vpc_id              = aws_vpc.myapp-vpc.id
+  vpc_id              = module.vpc.vpc_id
   my_ip_address       = var.my_ip_address
   env_prefix          = var.env_prefix
   image_name          = var.image_name
   public_key_location = var.public_key_location
   instance_type       = var.instance_type
-  subnet_id           = module.myapp-subnet.subnet.id
+  subnet_id           = module.vpc.public_subnets[0]
   avail_zone          = var.avail_zone
 }
